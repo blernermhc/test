34101bc235e11db910ee186fc315ee2455f3016e
Barbara Lerner <blerner@mtholyoke.edu>
2018-08-14 14:47:12 -0400

    Updated build script.

--- build_1.sh
+++ build_1.sh
@@ -1,33 +1,48 @@
 #!/bin/bash
 
-# Make sure both repos are on master
-git checkout master
-cd ../test_1
-git checkout master
-cd ../test
+function copy_changes {
+  TO_REPO=$1
+  BRANCH=$2
+  
+  echo TO_REPO = $TO_REPO
+  echo BRANCH = $BRANCH
+  
+  # Make sure both repos are on master
+  git checkout $BRANCH
+  cd ../$TO_REPO
+  git checkout $BRANCH
+  cd ../test
 
-# Get the latest commit number
-COMMIT=$(git show | head -n 1)
-echo COMMIT = $COMMIT
+  # Get the latest commit number
+  COMMIT=$(git show | head -n 1)
+  echo COMMIT = $COMMIT
 
-# Compare to the commit # stored in test_1
-COMMIT_1=$(cat ../test_1/.commit)
-echo COMMIT_1 = $COMMIT_1
+  # Compare to the commit # stored in $TO_REPO
+  COMMIT_1=$(cat ../$TO_REPO/.commit)
+  echo COMMIT_1 = $COMMIT_1
 
-# If they are the same we are done
-if [ "$COMMIT" == "$COMMIT_1" ] 
-  then
-    echo "test_1 master is current."
-    exit
-fi
+  # If they are the same we are done
+  if [ "$COMMIT" == "$COMMIT_1" ] 
+    then
+      echo "$TO_REPO $BRANCH is current."
+      exit
+  fi
+
+  # Copy the files we want to test_1
+  cp -p file.txt ../$TO_REPO/
+  cp -p file1.txt ../$TO_REPO/
+  echo $COMMIT > ../$TO_REPO/.commit
+
+  # Switch to $TO_REPO repo and commit changes
+  cd ../$TO_REPO
+  git add -A
+  git commit -m "Commit"
+  git push
+  
+  # Switch back to original directory
+  cd ../test
+}
+
+copy_changes "test_1" "master"
 
 
-# Copy the files we want to test_1
-cp -p file.txt ../test_1/
-cp -p file1.txt ../test_1/
-echo $COMMIT > ../test_1/commit
-
-# Switch to test_1 repo and commit changes
-git add -A
-git commit -m "Commit"
-git push
